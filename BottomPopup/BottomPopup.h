4554d7c0219b9b80116e61ea80d2ec808d7c61f3
wanny.park <wanny.park@pandora.tv>
2024-03-26 14:17:27 +0900

    cocoapods 설정 제거 / Carthage 설정 추가

---
+++ BottomPopup/BottomPopup.h
@@ -0,0 +1,18 @@
+//
+//  BottomPopup.h
+//  BottomPopup
+//
+//  Created by manaes on 3/26/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BottomPopup.
+FOUNDATION_EXPORT double BottomPopupVersionNumber;
+
+//! Project version string for BottomPopup.
+FOUNDATION_EXPORT const unsigned char BottomPopupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BottomPopup/PublicHeader.h>
+
+
